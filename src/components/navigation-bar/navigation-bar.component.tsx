d8ee182d999dc45996ec58d8a601c17d5ab67117
dongjoon min <dj.min43@gmail.com>
2024-03-01 18:18:26 +0900

    remove classnames

--- src/components/navigation-bar/navigation-bar.component.tsx
+++ src/components/navigation-bar/navigation-bar.component.tsx
@@ -1,12 +1,11 @@
 import styles from "./navigation-bar.module.scss";
-import classNames from "classnames";
 import { NavigationLink } from "@/components/common/navigation-link/navigation-link.component";
 import { routes } from "@/constants/routes";
 
 export const NavigationBar = () => {
   return (
-    <nav className={classNames(styles.navigationContainer)}>
-      <span className={classNames(styles.title)}>Summ(i:T)</span>
+    <nav className={styles.navigationContainer}>
+      <span className={styles.title}>Summ(i:T)</span>
       <div className={styles.navigationLinkContainer}>
         <NavigationLink href={routes.index}>home</NavigationLink>
         <NavigationLink href={routes.about}>about</NavigationLink>

924c5cf2a31f7c0da56d7525e629c6ea8df02a3f
dongjoon min <dj.min43@gmail.com>
2024-03-02 21:08:05 +0900

    add svgr

--- src/components/navigation-bar/navigation-bar.component.tsx
+++ src/components/navigation-bar/navigation-bar.component.tsx
@@ -1,11 +1,16 @@
+"use client";
+
 import styles from "./navigation-bar.module.scss";
 import { NavigationLink } from "@/components/common/navigation-link/navigation-link.component";
 import { routes } from "@/constants/routes";
+import Hamburger from "@material-design-icons/svg/outlined/menu.svg";
+import Image from "next/image";
 
 export const NavigationBar = () => {
   return (
     <nav className={styles.navigationContainer}>
       <span className={styles.title}>Summ(i:T)</span>
+      <Hamburger />
       <div className={styles.navigationLinkContainer}>
         <NavigationLink href={routes.index}>home</NavigationLink>
         <NavigationLink href={routes.about}>about</NavigationLink>

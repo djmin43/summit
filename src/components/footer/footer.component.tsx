64490b6e87f0dcd59e6b6234566fa56f243391dc
dongjoon min <dj.min43@gmail.com>
2024-03-03 11:02:57 +0900

    footer

--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -1,9 +1,23 @@
 import styles from "./footer.module.scss";
+import { NavigationLink } from "@/components/common/navigation-link/navigation-link.component";
+import { routes } from "@/constants/routes";
 import { Logo } from "@/components/common/logo/logo.component";
+
 export const Footer = () => {
   return (
     <footer className={styles.footerContainer}>
-      <Logo />
+      <div className={styles.footerNavigationContainer}>
+        <NavigationLink href={routes.index}>home</NavigationLink>
+        <NavigationLink href={routes.about}>about</NavigationLink>
+        <NavigationLink href={routes.portfolio}>portfolio</NavigationLink>
+        <NavigationLink href={routes.contact}>contact</NavigationLink>
+      </div>
+      <div className={styles.footerLogoContainer}>
+        <Logo />
+        <span className={styles.copyright}>
+          © 2024-present Summit. All Rights Reserved.
+        </span>
+      </div>
     </footer>
   );
 };

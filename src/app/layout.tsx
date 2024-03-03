0912d7a54036e1c02db5a6c4cb8831ac5263fc2d
dongjoon min <dj.min43@gmail.com>
2024-03-03 16:04:25 +0900

    og metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,27 @@ import { Footer } from "@/components/footer/footer.component";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SI Solutions",
-  description: "SI Solutions",
+  title:
+    "Summit - We bring sustainable and scalable solutions to your doorstep.",
+  description: "We bring sustainable and scalable solutions to your doorstep.",
+  applicationName: "Summit",
+  authors: [{ name: "Summit", url: "https://summit-chi.vercel.app/" }],
+  generator: "Summit",
+  keywords: "summit, system integration",
+  openGraph: {
+    type: "website",
+    url: "https://summit-chi.vercel.app/",
+    title:
+      "Summit - We bring sustainable and scalable solutions to your doorstep.",
+    description:
+      "We bring sustainable and scalable solutions to your doorstep.",
+    siteName: "Summit",
+    images: [
+      {
+        url: "https://thumb2.photo.mybox.naver.com/3472535967276863304?type=m1280_1280_2&nocache=4001160303",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({

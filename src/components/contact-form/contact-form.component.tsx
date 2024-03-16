1a0d79efbc7fe8f6718128225c8e0298ef3aca51
dongjoon min <dj.min43@gmail.com>
2024-03-16 18:38:37 +0900

    add form

--- src/components/contact-form/contact-form.component.tsx
+++ src/components/contact-form/contact-form.component.tsx
@@ -1,13 +1,29 @@
+"use client";
 import styles from "./contact-form.module.scss";
 import React from "react";
 import { Input } from "@/components/common/input/input.component";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+const contactFormSchema = z.object({
+  name: z.string().min(4).max(30),
+  email: z.string().email(),
+  inquiry: z.string().min(1).max(2000),
+});
+
+type ContactFormType = z.infer<typeof contactFormSchema>;
 
 export const ContactForm = () => {
+  const { register } = useForm<ContactFormType>({
+    resolver: zodResolver(contactFormSchema),
+  });
+
   return (
     <form className={styles.formContainer}>
       <div>
         <label htmlFor="name">name</label>
-        <Input id="name" placeholder="Name" />
+        <Input id="name" placeholder="Name" {...register("name")} />
       </div>
       <button type="submit">Submit</button>
     </form>
